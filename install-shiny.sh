3d58a84eda6aa3cc36e30f20d8b46e7aff521000
deividsanches <33764922+deividsanches@users.noreply.github.com>
2018-06-26 17:26:18 -0300

    Update install-shiny.sh

--- install-shiny.sh
+++ install-shiny.sh
@@ -2,6 +2,10 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
 sudo apt-get update
 sudo apt-get -y install r-base
+wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.5.872-amd64.deb
+md5sum shiny-server-1.5.5.872-amd64.deb
+sudo apt-get -y install gdebi-core
+
 
 sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('shinydashboard', repos = 'http://cran.rstudio.com/')\""
