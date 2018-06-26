9c0cd24ee7d4f79f2a20bd1a70647259829f993f
deividsanches <33764922+deividsanches@users.noreply.github.com>
2018-06-26 17:02:09 -0300

    Update install-shiny.sh

--- install-shiny.sh
+++ install-shiny.sh
@@ -1,7 +1,7 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu xenial/'
 sudo apt-get update
-sudo apt-get install r-base
+sudo apt-get -y install r-base
 
 sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('shinydashboard', repos = 'http://cran.rstudio.com/')\""
