182d48066f8befbeba2da6a9cc4f5bb9d84d9347
deividsanches <33764922+deividsanches@users.noreply.github.com>
2018-07-10 19:09:42 -0300

    Update deploy-dashboard-sudo.sh

--- deploy-dashboard-sudo.sh
+++ deploy-dashboard-sudo.sh
@@ -18,6 +18,13 @@ sudo wget https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.5.8
 sudo md5sum shiny-server-1.5.5.872-amd64.deb
 sudo gdebi shiny-server-1.5.5.872-amd64.deb -n
 
+#Install Node.js
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+npm cache clean -f
+npm install -g n
+n stable
+
 # Install required packages for Dashboard
 sudo su - -c "R -e \"install.packages('shiny', repos='http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"install.packages('shinydashboard', repos = 'http://cran.rstudio.com/')\""
@@ -34,26 +41,33 @@ sudo su - -c "R -e \"install.packages('plotly', repos = 'http://cran.rstudio.com
 sudo su - -c "R -e \"install.packages('devtools', repos = 'http://cran.rstudio.com/')\""
 sudo su - -c "R -e \"devtools::install_github('tidyverse/ggplot2')\""
 
+#Creating directories 
+sudo mkdir /srv/auth-proxy
+sudo chown ubuntu:ubuntu /srv/auth-proxy -R
+
 #Dashboard Port Configurator
-echo "Informe qual porta deseja instalar o Dashboard" 
-read port
-  if [ "$port" ] ; then
-         echo "Porta $port Escolhida " 
-    else
-sudo sed  -i "s/3838/$port/g" /etc/shiny-server/shiny-server.conf
-  fi
-sudo sed  -i "s/3838/$port/g" /etc/shiny-server/shiny-server.conf
+#echo "Informe qual porta deseja instalar o Dashboard" 
+#read port
+#  if [ "$port" ] ; then
+#         echo "Porta $port Escolhida " 
+#    else
+#sudo sed  -i "s/3838/$port/g" /etc/shiny-server/shiny-server.conf
+#  fi
+#sudo sed  -i "s/3838/$port/g" /etc/shiny-server/shiny-server.conf
 
 # Clone Dashboard Repository
-cd /srv/shiny-server/
-sudo git clone https://git-codecommit.sa-east-1.amazonaws.com/v1/repos/sharecare-rshiny-dashboard
-sudo systemctl restart shiny-server
+#cd /srv/shiny-server/
+#sudo git clone https://git-codecommit.sa-east-1.amazonaws.com/v1/repos/sharecare-rshiny-dashboard
+#sudo systemctl restart shiny-server
+
+
+
 
 #Configure Apache Server
-sudo a2enmod proxy
-sudo a2enmod proxy_http
+#sudo a2enmod proxy
+#sudo a2enmod proxy_http
 
 #sudo egrep -v "^#|^$"  /etc/apache2/sites-available/000-default.conf >  /etc/apache2/sites-available/000-default.conf
-sudo mkdir passwd
-sudo touch /etc/apache2/passwd/.htpasswd
-sudo service apache2 restart
+#sudo mkdir passwd
+#sudo touch /etc/apache2/passwd/.htpasswd
+#sudo service apache2 restart
